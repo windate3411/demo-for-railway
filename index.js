f2fa8cca913a701bc8edc73f55250f52aab81150
Danny Wang <dannywang@DannydeMBP.home>
2022-10-10 15:56:29 +0800

    udpate testing

--- index.js
+++ index.js
@@ -5,6 +5,13 @@ app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
+app.get("/login", (req, res) => {
+  res.json({
+    name: "Danny",
+    id: 9831032,
+  });
+});
+
 app.listen(process.env.PORT || 3000, () => {
   console.log("Express server start");
 });
